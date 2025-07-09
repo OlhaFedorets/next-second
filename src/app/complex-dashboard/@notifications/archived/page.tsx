376effabf47c3b0e52afef43f3789aa98b952c5b
OlhaFedorets <fedoretsolha@gmail.com>
2025-07-09 14:08:17 +0300

    add complex-dashboard adv archive

---
+++ src/app/complex-dashboard/@notifications/archived/page.tsx
@@ -0,0 +1,11 @@
+import Card from "@/components/card";
+import Link from "next/link";
+
+export default function ArchivedNotifications() {
+    return <Card>
+        <div className="flex flex-col">
+            <h2>Архив объявлений</h2>
+            <Link href="/complex-dashboard">Активные</Link>
+        </div>
+    </Card>
+}
